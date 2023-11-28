6342e7d08e67a997ed02759aaca59a77d0d6e2ba
Tal KRIEF <talkrief@gmail.com>
2023-11-28 14:33:17 +0100

    refacto done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'tasks#index'
-
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
-
+  resources :tasks
 end
